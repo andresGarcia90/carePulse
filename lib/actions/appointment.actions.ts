0ebf7303d989228019f24ae75b4fc1bb7af4634f
Andres Garcia <amgarciaamado@hipotecarioseguros.com>
2024-07-19 07:54:09 -0300

    Success Create Appointment Page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,4 +1,4 @@
-'use server'
+'use server';
 
 import { ID, InputFile, Query } from 'node-appwrite';
 import {
@@ -8,17 +8,29 @@ import {
 } from '../appwrite.config';
 import { parseStringify } from '../utils';
 
-export async function createAppointment(appointment: AppointmentParams){
-  try{
+export async function createAppointment(appointment: AppointmentParams) {
+  try {
     const newAppointment = await databases.createDocument(
       DATABASE_ID!,
       APPOINTMENT_COLLECTION_ID!,
       ID.unique(),
       appointment
-    )
-    return parseStringify(newAppointment)
-  } catch(error){
-    console.log(error)
+    );
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
   }
+}
 
+export async function getAppointment(appointmentId: string) {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
 }
